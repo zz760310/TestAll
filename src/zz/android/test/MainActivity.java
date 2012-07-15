a37224d401ca52c126ada3faeaefac2baecdfe47
zz760310 <zz760310@gmail.com>
2012-07-15 16:25:22 +0800

    first commit

---
+++ src/zz/android/test/MainActivity.java
@@ -0,0 +1,24 @@
+package zz.android.test;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.support.v4.app.NavUtils;
+
+public class MainActivity extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_activity);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_activity, menu);
+        return true;
+    }
+
+    
+}
